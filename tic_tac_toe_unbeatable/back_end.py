5e9b900038238d8e28634dc1b7a0beeb59438b07
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-03-25 10:28:32 +0200

    unbeatable tic tac toe

---
+++ tic_tac_toe_unbeatable/back_end.py
@@ -0,0 +1,69 @@
+import random
+
+class Back_End:
+
+	def __init__(self):
+		self.board = ['#','1','2','3','4','5','6','7','8','9']
+
+	def print_board(self):
+			print(' | '.join(self.board[1:4]) + '\n'
+				+ ' | '.join(self.board[4:7]) + '\n'
+				+ ' | '.join(self.board[7:10]))
+
+	def available_spaces(self):
+		return [index+1 for index,element in enumerate(self.board[1:]) if element not in ['X','O']]
+
+	def play_with(self):
+		comp_or_player = '#'
+		first_or_second = '#'
+		while comp_or_player == '#':
+			comp_or_player = input('Would you like to play with another player or computer? (P/C): ')
+			if comp_or_player[0].lower() == 'p':
+				return 'Player'
+			elif comp_or_player[0].lower() == 'c':
+				while first_or_second == '#':
+					first_or_second = input('Would you like to go first? (Y/N): ')
+					if first_or_second[0].lower() == 'y':
+						return 'First'
+					elif first_or_second[0].lower() == 'n':
+						return 'Second'
+					else:
+						print("Wrong Input! Please enter 'Y' to go first or 'N' to go second: ")
+						first_or_second = '#'
+			else:
+				print("Wrong Input! Please enter 'P' for player or 'C' for computer")
+				comp_or_player = '#'
+
+	def player_input(self,game,letter):
+		position = False
+		while not position:
+			try:
+				position = int(input('Please choose your next move (1-9): '))
+			except ValueError:
+				print('Wrong Input! Please try again!')
+			else:
+				if position not in game.available_spaces():
+					print('Current position is taken! Choose another one!')
+					position = False
+				else:
+					self.board[position] = letter
+
+	def computer_move(self,game,letter):
+		number = False
+		while not number:
+			number = random.randint(1,9)
+
+			if number not in game.available_spaces():
+				number = False
+		self.board[number] = letter
+
+	def winner_check(self,letter):
+		if (self.board[1:4] == [letter,letter,letter] or
+			self.board[4:7] == [letter,letter,letter] or
+			self.board[7:10] == [letter,letter,letter] or
+			self.board[1:8:3] == [letter,letter,letter] or
+			self.board[2:9:3] == [letter,letter,letter] or
+			self.board[3:10:3] == [letter,letter,letter] or
+			self.board[1:10:4] == [letter,letter,letter] or
+			self.board[3:8:2] == [letter,letter,letter]):
+			return True
---
+++ tic_tac_toe_unbeatable/main.py
@@ -0,0 +1,61 @@
+import back_end
+import time
+
+def tic_tac_toe():
+	while True:
+		game_on = True
+		back = back_end.Back_End()
+		letter = 'X'
+		player_or_computer = back.play_with()
+		print(player_or_computer)
+
+		while game_on == True:
+			if letter == 'X':
+				back.print_board()
+
+				if player_or_computer == 'Player' or player_or_computer == 'First':
+					back.player_input(back,letter)
+				elif player_or_computer == 'Second':
+					print('Computer is choosing next move...')
+					time.sleep(0.8)
+					back.computer_move(back,letter)
+
+				if back.winner_check(letter) == True:
+					back.print_board()
+					print('Player {} won the game!'.format(letter))
+					break
+				if len(back.available_spaces()) > 0:
+					letter = 'O'
+				else:
+					back.print_board()
+					print('It is a tie!')
+					break
+
+			if letter == 'O':
+				back.print_board()
+
+				if player_or_computer == 'Player' or player_or_computer == 'Second':
+					back.player_input(back,letter) # input
+				elif player_or_computer == 'First':
+					print('Computer is choosing next move...')
+					time.sleep(0.8)
+					back.computer_move(back,letter)
+
+				if back.winner_check(letter) == True:
+					back.print_board()
+					print('Player {} won the game!'.format(letter))
+					break
+				if len(back.available_spaces()) > 0:
+					letter = 'X'
+				else:
+					back.print_board()
+					break
+
+		ask = input('Would you like to play again? (Y/N): ').lower()
+		if ask[0] == 'y':
+			game_on = True
+		else:
+			break
+
+if __name__=='__main__':
+	tic_tac_toe()
