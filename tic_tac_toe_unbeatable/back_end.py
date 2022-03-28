b61c289659a89bf0f735f0a1787bc991032442a9
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-03-28 13:21:08 +0300

    unbeatable

--- tic_tac_toe_unbeatable/back_end.py
+++ tic_tac_toe_unbeatable/back_end.py
@@ -52,35 +52,40 @@ class Back_End:
 					self.board[position] = letter
 
 	def computer_move(self,letter):
+		save_board = []
+
 		if len(self.available_spaces()) > 7:
 			square = random.choice(self.available_spaces())
 			self.board[square] = letter
 		else:
 			# get the square based off the minimax algorithm
-			square = self.minimax(letter)['position']
+			square = self.minimax(letter,letter)['position']
 			self.board[square] = letter
 
-	def minimax(self,player):
-		max_player = player # yourself
+	def minimax(self,player,letter):
+		max_player = letter # yourself
 		other_player = 'O' if player == 'X' else 'X' # the other player
 
 		# check if the previous move is a winner
-		if self.current_winner == max_player:
-			return {'position': None,
-					'score': 1 * (len(available_spaces())+1) if other_player == max_player else 
-						-1 * (len(available_spaces())+1)
-					}
+		if self.current_winner == other_player:
+			return {'position': None,'score': 1 * (len(self.available_spaces())+1) if other_player == max_player else -1 * (len(self.available_spaces())+1)}
 		elif len(self.available_spaces()) == 0: # not empty squares
-			return {'position': None, 'score': 0}
+			return {'position': None,'score': 0}
 
 		if player == max_player:
-			best = {'position':None,'score': -math.inf} # each score should maximize
+			best = {'position': None, 'score': -math.inf}  # each score should maximize
 		else:
-			best = {'position':None,'score': math.inf} # each score should minimize
+			best = {'position': None, 'score': math.inf}  # each score should minimize
 
 		for possible_move in self.available_spaces():
+			# MAKE MOVE
 			self.board[possible_move] = player
-			sim_score = self.minimax(other_player)
+			if self.winner_check(player) == True:
+				self.current_winner = player
+
+			sim_score = self.minimax(other_player,letter)
+
+			# UNDO MOVE
 			self.board[possible_move] = self.save_board[possible_move]
 			self.current_winner = None
 			sim_score['position'] = possible_move
